3ea71ee82cd21b37c576b7b00b20747d3f70e182
Gabriela <103141996+GabrielaFiona@users.noreply.github.com>
2025-11-27 11:18:26 -0800

    Add script to display current year and change text color

---
+++ script.js
@@ -0,0 +1,5 @@
+// This MUST run if JS loads
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Change text color to confirm JS works
+document.getElementById("demo").style.color = "green";

f8be5c7191e6cdcc9d52013e007cb238b60161b5
Gabriela <103141996+GabrielaFiona@users.noreply.github.com>
2025-11-27 11:48:49 -0800

    Create page2.js

---
+++ page2.js
@@ -0,0 +1,5 @@
+// Auto-fill year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Change text via JS (to confirm JS loads)
+document.getElementById("message").style.color = "blue";
